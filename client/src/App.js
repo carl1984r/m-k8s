394a0708a59e1b457befe856ed8eb7cf57d679f0
Brylynn Cox <carl.cc@gmail.com>
2021-10-13 16:47:11 -0500

    Test image update

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Bry's K8S Cluster Test 
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t carl1984a0/m-client-0:latest -t carl1984a0/m-client-0:$SHA -f ./client/Dockerfile ./client
 docker build -t carl1984a0/m-server-0:latest -t carl1984a0/m-server-0:$SHA -f ./server/Dockerfile ./server
 docker build -t carl1984a0/m-worker-0:latest -t carl1984a0/m-worker-0:$SHA -f ./worker/Dockerfile ./worker
+
 docker push carl1984a0/m-client-0:latest
 docker push carl1984a0/m-server-0:latest
 docker push carl1984a0/m-worker-0:latest
